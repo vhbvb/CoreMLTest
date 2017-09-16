fff4ae7df2d6e60c6a4d3f3a647d9d1ed2fa04fb
Ever <ever@EverdeMacBook-Pro.local>
2017-09-16 22:23:26 +0800

    Initial Commit

---
+++ CoreMLTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLTest
+//
+//  Created by Ever on 2017/9/16.
+//  Copyright © 2017年 Ever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
